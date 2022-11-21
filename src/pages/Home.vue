e185e7a349d9993cf89ebd5c7b2dd38a92f7e56f
ChinnapatKhemthong <112239328+ChinnapatKhemthong@users.noreply.github.com>
2022-11-21 10:04:27 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+       <navbar />
+       <section class="home">
+   
+   <div class="image">
+       <img src="images/profile.png" alt="">
+   </div>
+   
+   <div class="content">
+       <h3>"FOOT FRUIT FOOD"</h3>
+       <span>"Fastest Delivery & Easy Pickup"</span>
+       <p>ร้านอาหารของเราเป็นร้านที่ มีอาหารหลากหลายรูปเเบบ หลากหลายสัญชาติในร้านเดียว เเละ มีราคาที่ย่อมเยาว์ ด้วยระบบสั่งอาหารออนไลน์ก็จะทําทุกท่านได้เลือกอาหารที่อยากทานด้วยปลายนิ้ว เเละรอรับที่บ้านของท่าน S I M P L E</p>
+       
+   
+   </div>
+   <div class="content">
+       <p> <span>Fast Delivery : </span> Promise to deliver within 30 mins </p>
+       <p> <span>Easy Pickup : </span> Easy order & easy life </p>
+       <p> <span>Good food :  </span> Food that makes you say wow.  </p>
+       <p> <span>Fresh food :   </span>Enjoy your food fresh crispy and hot. 	</p>
+       <p> <span>Promotion :  </span>There are a lot of foods promotion. </p>
+   </div>
+   
+   </section>
+    </div>
+   </template>
+   
+   <script>
+   import Navbar from '../components/Navbar.vue';
+   import axios from 'axios'
+   
+   
+   export default {
+       components: { Navbar },
+       methods: {
+           async fetchExperience() {
+               try {
+                   let res = await axios.get("https://633fc566e44b83bc73c0f545.mockapi.io/profile/v1/experiences")
+                   this.experience = res.data
+               } catch (error) {
+           
+               }
+           }
+            
+       },
+       data() { 
+           return { 
+               experience: []
+           }
+       },
+       beforeMount() {
+           this.fetchExperience()
+       }
+   
+               
+       } 
+   
+   </script>
+   
+   <style>
+   
+   </style>
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue"
+import About from "../pages/About.vue"
+import Foodweb from "../pages/Foodweb.vue"
+import Drink from "../pages/Drink.vue"
+
+const routes = [{
+    path: "/",
+    component: Home,
+    name: "Home",
+},{
+    path: "/about",
+    component: About,
+    name: "About",
+},{
+  path: "/Foodweb",
+  component: Foodweb,
+  name: "Foodweb",
+},{
+  path: "/Drink",
+  component: Drink,
+  name: "Drink",
+
+}]
+
+const router = createRouter({
+    history: createWebHistory(),
+    linkActiveClass: "active",
+    scrollBehavior(to, from, savedPosition) {
+      if (to.hash) {
+        return {
+          selector: to.hash,
+          behavior: 'smooth'
+        };
+      }
+      return { x: 0, y: 0 };  // Go to the top of the page if no hash
+    },
+    routes,
+  });
+  
+  export default router;
+
+  
